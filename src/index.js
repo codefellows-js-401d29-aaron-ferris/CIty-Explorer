9fdca85491e37489152981ac5b5e7c758c779be3
abferris <abferris@gmail.com>
2019-04-28 23:46:40 -0700

    did all I could. I had extenuating circumstances that kept me from finishing. Left a message with john

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <App />;
+  }
+}
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));
